983256f3e643a3aa63c235e4f8410ad8acdd054f
kacpe <kacper.radziwon@gmail.com>
2024-07-06 19:32:37 +0200

    basic profiles list

--- basic/views.py
+++ basic/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
-
+from .models import Profile
+from django.shortcuts import redirect
 # Create your views here.
 def home(request):
     return render(request, 'home.html', {})
+
+
+def profile_list(request):
+    if request.user.is_authenticated:
+        profiles= Profile.objects.exclude(user=request.user)
+        return render(request, 'profile_list.html', {"profiles": profiles})
+    else:
+        return redirect(home)
