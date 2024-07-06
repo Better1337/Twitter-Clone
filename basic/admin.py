ed29d577a090856e91ef3e9b69e996219e51607d
kacpe <kacper.radziwon@gmail.com>
2024-07-06 17:42:26 +0200

    auto create user profile after adding new user

--- basic/admin.py
+++ basic/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.models import Group, User
 from .models import Profile
 
+admin.site.unregister(Group)
 class ProfileInLine(admin.StackedInline):
     model = Profile
 
--- basic/models.py
+++ basic/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.db.models.signals import post_save
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     follows = models.ManyToManyField('self', related_name='followed_by', symmetrical=False, blank=True)
@@ -8,3 +8,11 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+def user_did_save(sender, instance, created, **kwargs):
+    if created:
+        user_profile = Profile(user=instance)
+        user_profile.save()
+        user_profile.follows.set([instance.profile.id])
+        user_profile.save()
+post_save.connect(user_did_save, sender=User)
